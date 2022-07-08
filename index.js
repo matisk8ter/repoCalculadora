5ec4aba3155a9f74db163444bfbc69a85198788b
Matias <matisouzafr@gmail.com>
2022-07-08 14:06:44 -0300

    segundo commit

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ divOperaciones.style.width = "90px"
 divOperaciones.style.height = "60px"
 divOperaciones.style.background = "white"
 divOperaciones.style.margin = "auto"
-
+divOperaciones.style.fontSize= "50px"
+divOperaciones.style.textAlign= "center"
 contPadre.appendChild(nodoOperaciones)
 nodoOperaciones.appendChild(divOperaciones)
 //----------------------------------------------------
@@ -41,9 +42,13 @@ nodoOperaciones.appendChild(divOperaciones)
 let opSuma = divOperaciones.cloneNode();
 let opDivision = divOperaciones.cloneNode();
 let opMult = divOperaciones.cloneNode();
+divOperaciones.innerHTML="-"
 nodoOperaciones.appendChild(opSuma);
+opSuma.innerHTML= "+"
 nodoOperaciones.appendChild(opDivision);
+opDivision.innerHTML="/"
 nodoOperaciones.appendChild(opMult);
+opMult.innerHTML="*"
 //------------------------------------------------
 
 //------------------DIV PADRE QUE CONTIENE LOS NUMEROS Y EL IGUAL-----------
@@ -60,6 +65,7 @@ contPadre.appendChild(divNumeros)
 let divIgual = document.createElement("div")
 divIgual.style.height = "220px"
 divIgual.style.width = "100px"
+divIgual.style.display= "felx"
 divIgual.style.background = "red"
 
 //-------------DIV QUE CONTIENE LOS NUMEROS
@@ -78,6 +84,19 @@ divNumeros.appendChild(contNumeros)
 //----------------------------------
 //----------agrego el div que contiene el igual
 divNumeros.appendChild(divIgual)
+//------------------------Div boton es igual-----------------------------------
+let igual= document.createElement("div")
+igual.style.height="150px"
+igual.style.width="75px"
+igual.style.display="flex"
+igual.style.background="white"
+igual.style.alignItems="center"
+igual.style.justifyContent="center"
+igual.style.margin = "15px"
+igual.innerHTML= "="
+igual.style.textAlign="center"
+igual.style.fontSize="50px"
+divIgual.appendChild(igual)
 
 //----------------DIV PARA FILA PARA 3 NUM 
 let divPara3Num = document.createElement("div")
@@ -121,15 +140,45 @@ contNumeros.appendChild(divPara3NumFila4)
 
 //-----------CLONO EL DIV DE CADA NUM----
 let n1 = num.cloneNode();
+n1.innerHTML="1"
+n1.style.textAlign="center"
+n1.style.fontSize="25px"
 let n2 = num.cloneNode();
+n2.innerHTML="2"
+n2.style.textAlign="center"
+n2.style.fontSize="25px"
 let n3 = num.cloneNode();
+n3.innerHTML="3"
+n3.style.textAlign="center"
+n3.style.fontSize="25px"
 let n4 = num.cloneNode();
+n4.innerHTML="4"
+n4.style.textAlign="center"
+n4.style.fontSize="25px"
 let n5 = num.cloneNode();
+n5.innerHTML="5"
+n5.style.textAlign="center"
+n5.style.fontSize="25px"
 let n6 = num.cloneNode();
+n6.innerHTML="6"
+n6.style.textAlign="center"
+n6.style.fontSize="25px"
 let n7 = num.cloneNode();
+n7.innerHTML="7"
+n7.style.textAlign="center"
+n7.style.fontSize="25px"
 let n8 = num.cloneNode();
+n8.innerHTML="8"
+n8.style.textAlign="center"
+n8.style.fontSize="25px"
 let n9 = num.cloneNode();
+n9.innerHTML="9"
+n9.style.textAlign="center"
+n9.style.fontSize="25px"
 let n0 = num.cloneNode();
+n0.innerHTML="0"
+n0.style.textAlign="center"
+n0.style.fontSize="25px"
 
 divPara3Num.appendChild(n1)
 divPara3Num.appendChild(n2)
@@ -144,9 +193,6 @@ divPara3NumFila3.appendChild(n8)
 divPara3NumFila3.appendChild(n9)
 
 divPara3NumFila4.appendChild(n0)
-
-
-
 
 
 
